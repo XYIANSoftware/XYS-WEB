60c50bff9bdae6096f980e0dc2ef64c75e64c4b2
Afro104 <royjr104@gmail.com>
2024-08-09 18:46:21 -0700

    latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-regular-svg-icons/faFile";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
 import 'primereact/resources/themes/lara-dark-purple/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -14,9 +18,11 @@ export default function Home() {
         <PrimeReactProvider>
             <main>
                 {/* <MenuModel /> */}
+                <div className='flex flex-column p-6 justify-content-center text-center align-items-center header'>
 
+                </div>
                 <div
-                    className='flex flex-column p-6 justify-content-center text-center align-items-center gap-3'
+                    className='flex flex-column p-6 justify-content-center text-center align-items-center gap-3 mainBody'
                     style={{
                         backgroundImage: `url(${backgroundImage})`,
                         // backgroundSize: 'cover',
@@ -30,6 +36,39 @@ export default function Home() {
                         Site is under new construction<br></br> Please come back
                         later.
                     </p>
+                </div>
+                <div className='flex flex-column p-6 justify-content-center text-center align-items-center footer'>
+                    <div className="z-50">
+                        <ul>
+                            <li>
+                                <a className="linkedIn" href="https://www.linkedin.com/company/104131290/admin/dashboard/" target="_blank" rel="noopener noreferrer">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <FontAwesomeIcon className='iconLogoSizes' icon={faLinkedin} />
+                                </a>
+                            </li>
+                            <li>
+                                <a className="twitter"  href="" target="_blank" rel="noopener noreferrer">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <FontAwesomeIcon className='iconLogoSizes' icon={faXTwitter} />
+                                </a>
+                            </li>
+                            <li>
+                                <a className="github" href="https://github.com/XYIAN" target="_blank" rel="noopener noreferrer">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <FontAwesomeIcon className='iconLogoSizes' icon={faGithub} />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </main>
         </PrimeReactProvider>
