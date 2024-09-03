927eaaa8b058a84c4747a4ca3ef14f3ef59303fd
XYIAN <kylexyiandilbeck@gmail.com>
2024-09-03 14:56:00 -0700

    update teams page layout and privacy layout

--- src/app/team/components/PersonDisplay.tsx
+++ src/app/team/components/PersonDisplay.tsx
@@ -1,16 +1,33 @@
 import { MemberDetail } from '@/common';
+// import Image from 'next/image';
+import { Image } from 'primereact/image';
 import { Card } from 'primereact/card';
-import { useEffect, useState, ReactNode } from 'react';
-
+import '../../../common/styles/displayCard.scss';
 interface PersonDisplayProps {
     memberDetails: MemberDetail;
 }
 
 export const PersonDisplay = ({ memberDetails }: PersonDisplayProps) => {
     const { name, imageSrc, position, bio } = memberDetails;
+    const CardHeaderImage = () => {
+        return (
+            <div className='flex justify-content-center align-content-center'>
+                <Image
+                    alt={name}
+                    src={imageSrc}
+                    imageStyle={{ maxWidth: '100%', objectFit: 'cover' }}
+                />
+            </div>
+        );
+    };
     return (
-        <Card title={<h2>{name}</h2>} subTitle={bio}>
-            Filler
+        <Card
+            header={<CardHeaderImage />}
+            title={<h2>{name}</h2>}
+            subTitle={position}
+            className='w-full m-3 p-3 custom-card'
+        >
+            <p className='pt-4 pb-1'>{bio}</p>
         </Card>
     );
 };
--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -4,42 +4,19 @@ import { MenuModel } from '@/common/Menu/MenuModel';
 import { NextPage } from 'next';
 import { PrimeReactProvider } from 'primereact/api';
 import { PersonDisplay } from './components/PersonDisplay';
+import { TEAM_LIST } from './TEAM_LIST';
 
 interface Props {}
-
-const TEAM_INFORMATION: MemberDetail[] = [
-    {
-        name: 'Kyle Dilbeck',
-        id: 1,
-        imageSrc: 'filler',
-        position: 'Founder & Developer',
-        bio: 'example bio',
-    },
-    {
-        name: 'Rogelio Rodriguez',
-        id: 2,
-        imageSrc: 'filler',
-        position: 'Co Founder & Developer',
-        bio: 'example bio',
-    },
-    {
-        name: 'Joel Sandoval',
-        id: 3,
-        imageSrc: 'filler',
-        position: 'Sales & Marketing',
-        bio: 'example bio',
-    },
-];
 
 const Team: NextPage<Props> = ({}) => {
     return (
         <PrimeReactProvider>
             <main>
                 <MenuModel />
-                <div className='flex justify-content-center pt-6 flex-column'>
-                    <h1>Meet Our Team</h1>
+                <div className='flex justify-content-center pt-8 flex-column'>
+                    <h1 className='text-center'>Meet Our Team</h1>
                     <div className='flex flex-column p-2 justify-content-center align-items-center'>
-                        {TEAM_INFORMATION.map((person) => {
+                        {TEAM_LIST.map((person) => {
                             return (
                                 <PersonDisplay
                                     memberDetails={person}
