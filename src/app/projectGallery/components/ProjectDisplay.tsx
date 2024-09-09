c2264b90352cd8f9fff91029095545d5a5f8d1f3
XYIAN <kylexyiandilbeck@gmail.com>
2024-09-09 11:32:05 -0700

    update

--- src/app/projectGallery/components/ProjectDisplay.tsx
+++ src/app/projectGallery/components/ProjectDisplay.tsx
@@ -1,21 +1,27 @@
 import { ProjectModel } from '@/common';
-import { useEffect, useState, ReactNode, CSSProperties } from 'react';
-
+import Image from 'next/image';
 interface ProjectDisplayProps {
-    project: ProjectModel[] ;
+    project: ProjectModel[];
 }
 
 export const ProjectDisplay = ({ project }: ProjectDisplayProps) => {
-    //TODO use list to map projects
-    // const { description, id, imgSrc, title, url } = project;
-
     return (
-        <div className="gallery">
+        <div className='gallery'>
             {project.map((proj, index) => (
-                <span key={index} style={{ "--i": index + 1 } as React.CSSProperties}>
-                <a href={proj.url} target="_blank" rel="noopener noreferrer">
-                    <img src={proj.imgSrc} alt={`Gallery image ${index + 1}`} />
-                </a>
+                <span
+                    key={index}
+                    style={{ '--i': index + 1 } as React.CSSProperties}
+                >
+                    <a
+                        href={proj.url}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <Image
+                            src={proj.imgSrc ? proj.imgSrc : ''}
+                            alt={`Gallery image ${index + 1}`}
+                        />
+                    </a>
                 </span>
             ))}
         </div>
