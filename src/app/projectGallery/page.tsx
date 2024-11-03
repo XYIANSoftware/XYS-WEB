8a4dbd100c17e70e5e666d33fab31d2450ee13dd
XYIAN <kylexyiandilbeck@gmail.com>
2024-11-03 04:23:36 -0800

    update descriptions and urls

--- src/app/projectGallery/page.tsx
+++ src/app/projectGallery/page.tsx
@@ -10,50 +10,65 @@ const PROJECT_LIST: ProjectModel[] = [
         id: 1,
         title: 'XYIAN Software',
         description:
-            'This is an example of a basic description that should be about this long with minimal details. ',
+            'XYIAN Software Public web application built with nextjs & typescript',
         imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
         url: 'https://xyian.com/',
     },
     {
         id: 2,
-        title: 'Looks Delivered ',
+        title: 'Looks Delivered',
         description:
-            'This is an example of a basic description that should be about this long with minimal details. ',
+            'Buisness Portfolio and Booking system for a mobile salon buisness.',
         imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
-        url: 'https://looksdelivered.com.com/',
+        url: 'https://looksdelivered.com/',
     },
     {
         id: 3,
-        title: 'C# Template',
+        title: 'Personal Portfolio',
         description:
-            'This is an example of a basic description that should be about this long with minimal details. ',
+            'Basic personal site for things lik job applications, freelancing, blogging etc. ',
         imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
-        url: 'https://xyian.com/',
+        url: 'https://kyledilbeck.com/',
     },
     {
         id: 4,
-        title: 'Expo Template',
-        description:
-            'This is an example of a basic description that should be about this long with minimal details. ',
+        title: 'Company Site',
+        description: 'Basic company site without functions',
         imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
-        url: 'https://xyian.com/',
+        url: 'https://dilbeckandsons.com/',
     },
-    {
-        id: 5,
-        title: 'NextJS 15 Template',
-        description:
-            'This is an example of a basic description that should be about this long with minimal details. ',
-        imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
-        url: 'https://xyian.com/',
-    },
-    {
-        id: 6,
-        title: 'React Native Template',
-        description:
-            'This is an example of a basic description that should be about this long with minimal details. ',
-        imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
-        url: 'https://xyian.com/',
-    },
+    // {
+    //     id: 3,
+    //     title: 'C# Template',
+    //     description:
+    //         'This is an example of a basic description that should be about this long with minimal details. ',
+    //     imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
+    //     url: 'https://xyian.com/',
+    // },
+    // {
+    //     id: 4,
+    //     title: 'Expo Template',
+    //     description:
+    //         'This is an example of a basic description that should be about this long with minimal details. ',
+    //     imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
+    //     url: 'https://xyian.com/',
+    // },
+    // {
+    //     id: 5,
+    //     title: 'NextJS 15 Template',
+    //     description:
+    //         'This is an example of a basic description that should be about this long with minimal details. ',
+    //     imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
+    //     url: 'https://xyian.com/',
+    // },
+    // {
+    //     id: 6,
+    //     title: 'React Native Template',
+    //     description:
+    //         'This is an example of a basic description that should be about this long with minimal details. ',
+    //     imgSrc: '/images/XYIAN_BANNER.png', //use one from logo?
+    //     url: 'https://xyian.com/',
+    // },
     // {
     //     id: 7,
     //     title: '',
