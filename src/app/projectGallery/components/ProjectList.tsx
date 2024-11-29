da105666b2c59149ccd2963742a3d66847599f7b
XYIAN <kylexyiandilbeck@gmail.com>
2024-11-28 17:48:32 -0800

    accordion style fix

--- src/app/projectGallery/components/ProjectList.tsx
+++ src/app/projectGallery/components/ProjectList.tsx
@@ -1,39 +1,93 @@
 import { ProjectModel } from '@/common';
 import React from 'react';
-
 import { Accordion, AccordionTab } from 'primereact/accordion';
-
+import Image from 'next/image';
+import '../../../common/styles/accordionOverride.scss';
 interface ProjectListProps {
     projects: ProjectModel[];
 }
+
 interface AccordionHeaderProps {
     title: string;
-    iconUrl?: string;
+    iconUrl: string;
 }
-const ProjectList = ({ projects }: ProjectListProps) => {
-    //TODO create clean header
-    //TODO design tab layout + add thumbnail
-    const AccordionHeader = ({ title, iconUrl }: AccordionHeaderProps) => {
-        return (
-            <div className='flex align-items-center justify-content-center'>
-                <h5>{title}</h5>
-            </div>
-        );
-    };
+
+const AccordionHeader = ({ title, iconUrl }: AccordionHeaderProps) => {
     return (
-        <Accordion activeIndex={0}>
-            {projects.map((project) => {
-                return (
-                    <AccordionTab
-                        key={project.id}
-                        header={<AccordionHeader title={project.title} />}
-                    >
-                        <p>{project.description}</p>
-                        <a href={project.url}>Click Here to View</a>
-                    </AccordionTab>
-                );
-            })}
-        </Accordion>
+        <div className='flex align-items-center gap-3 p-2'>
+            {iconUrl && (
+                <Image
+                    src={iconUrl}
+                    alt={title}
+                    width={48}
+                    height={48}
+                    className='border-circle'
+                />
+            )}
+            <h5 className='m-0'>{title}</h5>
+        </div>
+    );
+};
+
+const ProjectList = ({ projects }: ProjectListProps) => {
+    const fillerImage = require('../../../../public/Primary.png');
+
+    return (
+        <div className='flex justify-content-center'>
+            <div style={{ width: '80%' }}>
+                <Accordion
+                    className='shadow-2 border-round'
+                    multiple
+                    //transitionOptions={{ easing: 'ease', duration: 300 }}
+                >
+                    {projects.map((project) => {
+                        const imageSource = project?.imgSrc
+                            ? project.imgSrc
+                            : fillerImage;
+                        return (
+                            <AccordionTab
+                                key={project.id}
+                                className=''
+                                header={
+                                    <AccordionHeader
+                                        title={project.title}
+                                        iconUrl={imageSource}
+                                    />
+                                }
+                            >
+                                <div className='p-3'>
+                                    <div className='flex flex-column md:flex-row align-items-start gap-4'>
+                                        <Image
+                                            src={imageSource}
+                                            alt={project.title}
+                                            width={50}
+                                            height={50}
+                                            className='border-round shadow-1'
+                                        />
+                                        <div>
+                                            <h6 className='mb-2'>
+                                                {project.title}
+                                            </h6>
+                                            <p className='text-justify'>
+                                                {project.description}
+                                            </p>
+                                            <a
+                                                href={project.url}
+                                                className='text-primary font-medium'
+                                                target='_blank'
+                                                rel='noopener noreferrer'
+                                            >
+                                                View Project
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </AccordionTab>
+                        );
+                    })}
+                </Accordion>
+            </div>
+        </div>
     );
 };
 
