84a8f58c826bd85553bf31f85fbb8aaeacb5d398
XYIAN <kylexyiandilbeck@gmail.com>
2024-12-04 14:30:37 -0800

    update team list pics

--- src/app/team/constants/TEAM_LIST.ts
+++ src/app/team/constants/TEAM_LIST.ts
@@ -2,14 +2,6 @@ import { MemberDetail } from '@/common';
 
 export const TEAM_LIST: MemberDetail[] = [
     {
-        name: 'Gabriel Villadpudua',
-        id: 4,
-        imageSrc:
-            'https://github.com/XYIANSoftware/images/blob/main/XYIAN_Primary.png?raw=true',
-        position: 'Sales & Marketing',
-        bio: 'Knack for driving growth, building strong client relationships, and crafting innovative strategies that boost brand visibility and revenue.',
-    },
-    {
         name: 'Joel Sandoval',
         id: 3,
         imageSrc:
@@ -18,10 +10,19 @@ export const TEAM_LIST: MemberDetail[] = [
         bio: 'Dynamic Sales and Marketing pro focused on driving growth and connecting with customers through innovative, results-oriented strategies.',
     },
     {
+        name: 'Gabriel Villadpudua',
+        id: 4,
+        imageSrc:
+            'https://media.licdn.com/dms/image/v2/D5603AQEUl7SOaeWDYg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725493937518?e=1738800000&v=beta&t=nxSldRV1sW40zaPqEru1687wGqlpZY9lx_ADB7X2Ghw',
+        position: 'Sales & Marketing',
+        bio: 'Knack for driving growth, building strong client relationships, and crafting innovative strategies that boost brand visibility and revenue.',
+    },
+
+    {
         name: 'Rogelio Rodriguez',
         id: 2,
         imageSrc:
-            'https://github.com/XYIANSoftware/images/blob/main/XYIAN_Primary.png?raw=true',
+            'https://github.com/XYIANSoftware/images/blob/main/IMG_9974.jpeg?raw=true',
         position: 'Co Founder & Developer',
         bio: 'Expertise in crafting robust software solutions, collaborating on product vision, and bringing ideas to life through code.',
     },
