5ca090311425c58ab69f8ca2f0d18711f50d5f4f
XYIAN <kylexyiandilbeck@gmail.com>
2024-12-21 23:57:52 -0800

    new customer page

---
+++ src/app/new/welcome/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome to XYS</h1>
+            <p>
+                Thank you for your interest in XYS! Keep an eye out, a member of
+                our team will contact you shortly using the email or phone
+                number you just used.
+            </p>
+        </div>
+    );
+};
