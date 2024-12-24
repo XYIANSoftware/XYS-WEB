36c318ee0946d8434217f27c99ec54e6d7e153c6
XYIAN <kylexyiandilbeck@gmail.com>
2024-12-24 10:40:08 -0800

    team list reorg

--- src/constants/TEAM_LIST.ts
+++ src/constants/TEAM_LIST.ts
@@ -2,23 +2,6 @@ import { MemberDetail } from '@/types';
 
 export const TEAM_LIST: MemberDetail[] = [
     {
-        name: 'Joel Sandoval',
-        id: 3,
-        imageSrc:
-            'https://media.licdn.com/dms/image/v2/C5603AQEBK58WD5IfhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516777231990?e=1738800000&v=beta&t=gH94QalvxNVUYHlZ1VmzAoUcHCAUpN-p88NYwWaFjig',
-        position: 'Sales & Marketing',
-        bio: 'Dynamic Sales and Marketing pro focused on driving growth and connecting with customers through innovative, results-oriented strategies.',
-    },
-    {
-        name: 'Gabriel Villadpudua',
-        id: 4,
-        imageSrc:
-            'https://media.licdn.com/dms/image/v2/D5603AQEUl7SOaeWDYg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725493937518?e=1738800000&v=beta&t=nxSldRV1sW40zaPqEru1687wGqlpZY9lx_ADB7X2Ghw',
-        position: 'Sales & Marketing',
-        bio: 'Knack for driving growth, building strong client relationships, and crafting innovative strategies that boost brand visibility and revenue.',
-    },
-
-    {
         name: 'Rogelio Rodriguez',
         id: 2,
         imageSrc:
@@ -34,5 +17,21 @@ export const TEAM_LIST: MemberDetail[] = [
             'https://github.com/XYIANSoftware/images/blob/main/kxd_logos/kx2.jpg?raw=true',
         position: 'Founder & Developer',
         bio: 'Passion for building cutting-edge solutions, driving technical innovation, and leading teams to deliver high-quality products.',
+    },
+    {
+        name: 'Gabriel Villadpudua',
+        id: 4,
+        imageSrc:
+            'https://media.licdn.com/dms/image/v2/D5603AQEUl7SOaeWDYg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725493937518?e=1738800000&v=beta&t=nxSldRV1sW40zaPqEru1687wGqlpZY9lx_ADB7X2Ghw',
+        position: 'Sales & Marketing',
+        bio: 'Knack for driving growth, building strong client relationships, and crafting innovative strategies that boost brand visibility and revenue.',
+    },
+    {
+        name: 'Joel Sandoval',
+        id: 3,
+        imageSrc:
+            'https://media.licdn.com/dms/image/v2/C5603AQEBK58WD5IfhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516777231990?e=1738800000&v=beta&t=gH94QalvxNVUYHlZ1VmzAoUcHCAUpN-p88NYwWaFjig',
+        position: 'Sales & Marketing',
+        bio: 'Dynamic Sales and Marketing pro focused on driving growth and connecting with customers through innovative, results-oriented strategies.',
     },
 ];
