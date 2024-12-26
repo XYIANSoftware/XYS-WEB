794284bc1a3381ddc975a15c7a62433f5ddcc467
XYIAN <kylexyiandilbeck@gmail.com>
2024-12-26 15:15:43 -0800

    skeleton store api

---
+++ src/API/store.tsx
@@ -0,0 +1,11 @@
+export const storeAPI = () => {
+    //*these will be axios calls for shopify api
+    const getItems = () => {
+        return;
+    };
+    const setItems = () => {};
+    const purchaseCart = () => {
+        return;
+    };
+    return { getItems, setItems, purchaseCart };
+};
