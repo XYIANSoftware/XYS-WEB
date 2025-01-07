6179dcd32baf1a8e58ff8b410493b828b0fddd04
XYIAN <kylexyiandilbeck@gmail.com>
2025-01-06 21:49:48 -0800

    hot fix store page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -5,8 +5,13 @@ interface Props {}
 
 const Store: NextPage<Props> = ({}) => {
     return (
-        <div className='h-full w-full justify-content-center align-items-center'>
-            <h1 className='p-2'>XYS Store</h1>
+        <div className='flex flex-column h-full w-full justify-content-center align-items-center p-1'>
+            <h1 className='p-2 text-center pt-6'>XYS Store</h1>
+            <p className='text-center p-3'>
+                XYS Store is currently under an inventory change and restock as
+                we prepare our new 2025 line. Please come back after 1/12/25, we
+                appriciate your patience and understanding.{' '}
+            </p>
         </div>
     );
 };
