8a548e476a667b1056ee555417d7be99e911f85b
XYIAN <kylexyiandilbeck@gmail.com>
2025-01-11 12:17:52 -0800

    temp-storr

---
+++ src/app/store/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+
+const Store = () => {
+    return (
+        <div className='pt-6'>
+            <h1 className='p-2'>Xyian Apparel&trade;</h1>
+            <p>
+                Store inventory in rotation, please come back later as we update
+                our new 2025 line.{' '}
+            </p>
+        </div>
+    );
+};
+
+export default Store;
