0656c272a5da1f74a926a6bfc3f58493e664e781
XYIAN <kylexyiandilbeck@gmail.com>
2025-01-11 11:34:49 -0800

    hotfix team list

--- src/app/team/constants/TEAM_LIST.ts
+++ src/app/team/constants/TEAM_LIST.ts
@@ -2,22 +2,13 @@ import { MemberDetail } from '@/types';
 
 export const TEAM_LIST: MemberDetail[] = [
     {
-        name: 'Joel Sandoval',
-        id: 3,
+        name: 'Kyle Dilbeck',
+        id: 1,
         imageSrc:
-            'https://media.licdn.com/dms/image/v2/C5603AQEBK58WD5IfhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516777231990?e=1738800000&v=beta&t=gH94QalvxNVUYHlZ1VmzAoUcHCAUpN-p88NYwWaFjig',
-        position: 'Sales & Marketing',
-        bio: 'Dynamic Sales and Marketing pro focused on driving growth and connecting with customers through innovative, results-oriented strategies.',
+            'https://github.com/XYIAN/headshots/blob/main/headshot%20-%208.png?raw=true',
+        position: 'Founder & Developer',
+        bio: 'Passion for building cutting-edge solutions, driving technical innovation, and leading teams to deliver high-quality products.',
     },
-    {
-        name: 'Gabriel Villadpudua',
-        id: 4,
-        imageSrc:
-            'https://media.licdn.com/dms/image/v2/D5603AQEUl7SOaeWDYg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725493937518?e=1738800000&v=beta&t=nxSldRV1sW40zaPqEru1687wGqlpZY9lx_ADB7X2Ghw',
-        position: 'Sales & Marketing',
-        bio: 'Knack for driving growth, building strong client relationships, and crafting innovative strategies that boost brand visibility and revenue.',
-    },
-
     {
         name: 'Rogelio Rodriguez',
         id: 2,
@@ -27,12 +18,21 @@ export const TEAM_LIST: MemberDetail[] = [
         position: 'Co Founder & Developer',
         bio: 'Expertise in crafting robust software solutions, collaborating on product vision, and bringing ideas to life through code.',
     },
+
     {
-        name: 'Kyle Dilbeck',
-        id: 1,
+        name: 'Gabriel Villadpudua',
+        id: 4,
         imageSrc:
-            'https://github.com/XYIANSoftware/images/blob/main/kxd_logos/kx2.jpg?raw=true',
-        position: 'Founder & Developer',
-        bio: 'Passion for building cutting-edge solutions, driving technical innovation, and leading teams to deliver high-quality products.',
+            'https://media.licdn.com/dms/image/v2/D5603AQEUl7SOaeWDYg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1725493937518?e=1738800000&v=beta&t=nxSldRV1sW40zaPqEru1687wGqlpZY9lx_ADB7X2Ghw',
+        position: 'Sales & Marketing',
+        bio: 'Knack for driving growth, building strong client relationships, and crafting innovative strategies that boost brand visibility and revenue.',
+    },
+    {
+        name: 'Joel Sandoval',
+        id: 3,
+        imageSrc:
+            'https://media.licdn.com/dms/image/v2/C5603AQEBK58WD5IfhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516777231990?e=1738800000&v=beta&t=gH94QalvxNVUYHlZ1VmzAoUcHCAUpN-p88NYwWaFjig',
+        position: 'Sales & Marketing',
+        bio: 'Dynamic Sales and Marketing pro focused on driving growth and connecting with customers through innovative, results-oriented strategies.',
     },
 ];
